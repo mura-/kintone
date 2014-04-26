ab7d95dd7a3373145e5b59deb922c401be9a9eb5
MURAHAMA <kamari.wmp@gmail.com>
2014-04-26 14:20:23 +0900

    test1

--- jsTest/test.js
+++ jsTest/test.js
@@ -4,7 +4,21 @@
 (function() {
      
     "use strict";
-     
-    alert("hello");
+    // レコード一覧の表示時にフィールド値の条件に応じて、文字色、フィールドの背景色を変更する
+    kintone.events.on('app.record.index.show', function (event) {
+ 
+        var elStatus = kintone.app.getFieldElements('会議種別');
+ 
+        for (var i = 0; i < elStatus.length; i++) {
+            var record = event.records[i];  
+ 
+            if (record['処理状況']['value'] == "PAGMTG") {
+                elStatus[i].style.color = 'red';
+                elStatus[i].style.backgroundColor = 'cornsilk';
+            }
+ 
+        }
+ 
+    });
  
 })();
