4143d7d290f76888e1a9440cff723ec68f19d154
MURAHAMA <kamari.wmp@gmail.com>
2014-04-30 22:50:45 +0900

    add test for login

---
+++ jsTest/TestForLogin.js
@@ -0,0 +1,28 @@
+/*
+ * サンプルカスタマイズ
+ */
+(function() {
+     
+    "use strict";
+    // レコード一覧の表示時にフィールド値の条件に応じて、文字色、フィールドの背景色を変更する
+    kintone.events.on('app.record.index.show', function (event) {
+//        var eluserId = kintone.app.getFieldElements('user_id');
+//        var elUserPw = kintone.app.getFieldElements('user_pw');
+//        var elUrgent = kintone.app.getFieldElements('Urgent');
+//
+        var apiPath = '/k/v1/records.json';
+        var httpMethod = 'GET';
+        var query = {query : 'user_id = ' + enterUserId};
+        var enterUserId = 'hogehoge';
+        var enterUserPw = 'foobar';
+
+
+        kintone.api(apiPath, httpMethod, query, function(resp) {
+
+            alert(resp);
+        });
+        
+
+    });
+
+})();
