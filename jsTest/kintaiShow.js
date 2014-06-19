e8f0ec3a0d440cf46008019d358d92beeec25874
MURAHAMA <kamari.wmp@gmail.com>
2014-06-20 07:33:12 +0900

    add google client load

--- jsTest/kintaiShow.js
+++ jsTest/kintaiShow.js
@@ -1,19 +1,12 @@
 (function(){
 
+
     var targetElementId = '#kintai-show';
     kintone.events.on('app.record.index.show', function (event) {
 
         main();
 
     });
-
-    /**
-     *
-     */
-    var createKintaiShowButton = function() {
-    
-    };
-
 
     /**
      * @constructor
@@ -24,29 +17,57 @@
          * @private
          */
         this.timerId_ = 0;
-
     };
 
     /**
      * 勤怠表示のエリアを作成する。
      */
     KintaiArea.prototype.addArea = function() {
-        $(targetElementId).html("test");
+        // $(targetElementId).append(this.createAuthorizeButton());
+        kintone.app.getHeaderMenuSpaceElement().appendChild(this.createAuthorizeButton());
+        var myHeaderSpace = kintone.app.getHeaderSpaceElement();
+        // 文字列要素
+        var myListHeaderDiv = document.createElement('div');
+        myListHeaderDiv.className = 'view-names';
     };
 
-    KintaiArea.prototype.isTargetUrl = function () {
-        if (targetUrl === location.pathname) {
-            return true;
-        }
-        return false;
-    }
+    /**
+     * googleAPI認証ボタン作成
+     */
+    KintaiArea.prototype.createAuthorizeButton = function() {
+        var authorizeButton = document.createElement('button');
+        authorizeButton.id = 'authorize-button';
+        authorizeButton.innerHTML = '認証する';
+        // authorizeButton.style.visibility = 'hidden';
+        return authorizeButton;
+    };
 
     function main() {
         var user = kintone.getLoginUser();
         if (user.code === 'k.murahama') {
-            var kintaiArea = new KintaiArea();
+            kintaiArea = new KintaiArea();
             kintaiArea.addArea();
         }
+    }
+
+    var KintaiData = function() {
+        this.kintaiDataJson;
+    };
+
+    var GApiManager = function() {
+        this.clientId = '311224808431-1ishkg2dbq3m45pedn3eeju43c3v5h7l.apps.googleusercontent.com';
+        this.apiKey = 'AIzaSyChnk6hoQdIKjfnq2XVJxwxUlv1KUh8kHU';
+        this.scopes = ['https://www.googleapis.com/auth/plus.me','https://www.googleapis.com/auth/calendar'];
+
+    };
+
+    // googleApi待ち受け
+    handleClientLoad = function() {
+        var gApiManager = new GApiManager();
+        // 予めAPI Consoleで設定したAPIキーを設定
+        gapi.client.setApiKey(gApimanager.apiKey);
+        // すでに認証済みかの確認をする。
+        //window.setTimeout(checkAuth,1);
     }
 
 })();
